f4bbb4abd39ec860cd290b4e4a9ce819bdc7c631
sornmonika <monika.sorn@institute.pse.ngo>
2025-03-03 14:15:57 +0700

    contact page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Contact from "../src/pages/contact"
+// import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Contact/>
+
   </StrictMode>,
 )
